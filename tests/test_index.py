c88331c07159376c4cd3bf8eac8b3289df63cc2a
abersheeran <me@abersheeran.com>
2021-03-06 15:21:00 +0800

    Update tests

--- tests/test_index.py
+++ tests/test_index.py
@@ -1,29 +1,27 @@
-from unittest import TestCase
-
 import pytest
 
 from typedmongo import Index
 
 
 def test_json():
-    TestCase().assertDictEqual(Index().value, {})
-    TestCase().assertDictEqual(Index(unique=True).value, {"unique": True})
-    TestCase().assertDictEqual(Index(sparse=True).value, {"sparse": True})
-    TestCase().assertDictEqual(Index(expire=600).value, {"expireAfterSeconds": 600})
-    TestCase().assertDictEqual(
-        Index(unique=True, sparse=True, expire=600).value,
-        {"unique": True, "sparse": True, "expireAfterSeconds": 600},
-    )
+    assert Index().value == {}
+    assert Index(unique=True).value == {"unique": True}
+    assert Index(sparse=True).value == {"sparse": True}
+    assert Index(expire=600).value == {"expireAfterSeconds": 600}
+    assert Index(unique=True, sparse=True, expire=600).value == {
+        "unique": True,
+        "sparse": True,
+        "expireAfterSeconds": 600,
+    }
 
 
 def test_keys():
-    TestCase().assertDictEqual(Index(keys={}).keys, {})
-    TestCase().assertDictEqual(Index(keys={"hello": 1}).keys, {"hello": 1})
-    TestCase().assertDictEqual(Index().add_key("hello", -1).keys, {"hello": -1})
+    assert Index(keys={}).keys == {}
+    assert Index(keys={"hello": 1}).keys == {"hello": 1}
+    assert Index().add_key("hello", -1).keys == {"hello": -1}
+
     # Not override able
-    TestCase().assertDictEqual(
-        Index().add_key("hello", -1).add_key("hello", 1).keys, {"hello": -1}
-    )
+    assert Index().add_key("hello", -1).add_key("hello", 1).keys == {"hello": -1}
 
     with pytest.raises(TypeError):
         Index(keys={"hello": 2})
